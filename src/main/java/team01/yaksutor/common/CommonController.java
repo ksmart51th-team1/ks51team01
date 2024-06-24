ccccac5807e4d2c91a89dab2dc056f3a84b4d2ec
SXYXH <bacxr1945@gmail.com>
2024-06-24 17:17:38 +0900

    feat: index.html 화면 수정

--- src/main/java/team01/yaksutor/common/CommonController.java
+++ src/main/java/team01/yaksutor/common/CommonController.java
@@ -1,14 +1,15 @@
 package team01.yaksutor.common;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CommonController {
 
     @GetMapping("/")
-    public String index() {
-
+    public String index(Model model) {
+        model.addAttribute("title", "한국스마트정보교육원 51기 1팀 약수터 프로젝트");
         return "index";
     }
 }
