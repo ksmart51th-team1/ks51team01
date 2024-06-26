20312aff4078b06200a8d85f5db9aa9c75ae2a81
SXYXH <bacxr1945@gmail.com>
2024-06-26 16:05:25 +0900

    feat : memberOwnerInsert.html, memberOwnerInsert.html 개발

---
+++ src/main/java/team01/yaksutor/pharmacy/member/dto/Member.java
@@ -0,0 +1,18 @@
+package team01.yaksutor.pharmacy.member.dto;
+
+import lombok.Data;
+
+@Data
+public class Member {
+    private String memberId;
+    private int memberLevelNum;
+    private String memberPw;
+    private String memberName;
+    private String memberGender;
+    private String memberEmail;
+    private String memberBirth;
+    private String memberPhone;
+    private String memberAddrSido;
+
+
+}
