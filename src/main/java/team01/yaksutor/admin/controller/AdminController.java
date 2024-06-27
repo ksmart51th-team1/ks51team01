ab8c59149cd5dfc1d2e5f2ddd558566378c869e6
Gil-seven <95398758+Gil-seven@users.noreply.github.com>
2024-06-27 10:50:14 +0900

    feat : admin medicine page 개발

--- src/main/java/team01/yaksutor/admin/controller/AdminController.java
+++ src/main/java/team01/yaksutor/admin/controller/AdminController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import team01.yaksutor.admin.dto.Efficacy;
+import team01.yaksutor.admin.dto.Ingredient;
 import team01.yaksutor.admin.dto.Medicine;
+import team01.yaksutor.admin.dto.SellMedicine;
 
 @Controller
 @RequestMapping("/admin")
@@ -23,18 +26,42 @@ public class AdminController {
         return "admin/adminMain";
     }
 
-    @GetMapping("/medicineInsert")
+    @GetMapping("/medicine/medicineInsert")
     public String medicineInsert(Model model) {
         model.addAttribute("title", "의약품 등록");
         model.addAttribute("content", "의약품 등록");
         return "admin/medicine/medicineInsert";
     }
 
-    @PostMapping("/medicineInsert")
-    public String medicineInsert(Medicine medicine) {
-
-
+    @PostMapping("/medicine/medicineInsert")
+    public String medicineInsert(Medicine medicine,
+                                 SellMedicine sellMedicine,
+                                 Ingredient ingredient,
+                                 Efficacy efficacy) {
+        log.info("의약품 등록 Medicine: {}", medicine);
+        log.info("의약품 등록 SellMedicine: {}", sellMedicine);
+        log.info("의약품 등록 Ingredient: {}", ingredient);
+        log.info("의약품 등록 Efficacy: {}", efficacy);
 
         return "redirect:/admin/adminMain";
+    }
+
+    @GetMapping("/medicine/medicineSearchList")
+    public String medicineSearchList(Model model) {
+        model.addAttribute("title", "의약품 리스트");
+        model.addAttribute("content", "의약품리스트");
+
+        return "admin/medicine/medicineSearchList";
+    }
+
+    @GetMapping("/medicine/medicineModify")
+    public String medicineModify(Model model) {
+
+        return "admin/medicine/medicineModify";
+    }
+
+    @GetMapping("/medicine/medicineDelete")
+    public String medicineDelete(Model model) {
+        return "admin/medicine/medicineDelete";
     }
 }
