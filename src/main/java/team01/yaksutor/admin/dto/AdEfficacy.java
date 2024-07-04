8ff477ebbcccbfe1357b54587ca7e2f01e71e919
Gil-seven <95398758+Gil-seven@users.noreply.github.com>
2024-07-04 16:21:14 +0900

    cache

---
+++ src/main/java/team01/yaksutor/admin/dto/AdEfficacy.java
@@ -0,0 +1,12 @@
+package team01.yaksutor.admin.dto;
+
+import lombok.Data;
+
+@Data
+public class AdEfficacy {
+    private String efficacyCode;
+    private String mediCode;
+    private String regMId;
+    private String efficacy;
+    private String regDate;
+}
---
+++ src/main/java/team01/yaksutor/admin/dto/AdIngredient.java
@@ -0,0 +1,13 @@
+package team01.yaksutor.admin.dto;
+
+import lombok.Data;
+
+@Data
+public class AdIngredient {
+    private String ingredientCode;
+    private String mediCode;
+    private String regMId;
+    private String ingredient;
+    private String content;
+    private String regDate;
+}
