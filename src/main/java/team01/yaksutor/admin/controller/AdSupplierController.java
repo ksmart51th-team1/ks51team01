059932bf75694e57b93bf7fc33746065b279e452
jyPark4321 <qkrwnddus93@gmail.com>
2024-07-09 17:32:14 +0900

    feat : 납품업체 회원추가

--- src/main/java/team01/yaksutor/admin/controller/AdSupplierController.java
+++ src/main/java/team01/yaksutor/admin/controller/AdSupplierController.java
@@ -1,14 +1,28 @@
 package team01.yaksutor.admin.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import team01.yaksutor.dto.Member;
+import team01.yaksutor.dto.Supplier;
 
 @Controller
 @RequestMapping("/admin/supplier")
+@RequiredArgsConstructor
+@Slf4j
 public class AdSupplierController {
+
+    @PostMapping("SupplierInsert")
+    public String supplierInsert(Member member, Supplier supplier){
+        log.info("member {}",member);
+        log.info("supplier {}",supplier);
+
+        return "redirect:/admin/supplier/supplierInsert";
+    }
 
 
 
