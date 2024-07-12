4aea0d9ca5343170eee9397ae420c63bb50f7816
jyPark4321 <qkrwnddus93@gmail.com>
2024-07-12 17:30:20 +0900

    feat : 로그인기록 전체리스트출력, 삭제

--- src/main/java/team01/yaksutor/admin/controller/AdLoginController.java
+++ src/main/java/team01/yaksutor/admin/controller/AdLoginController.java
@@ -5,8 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import team01.yaksutor.admin.service.AdLoginHistoryService;
 import team01.yaksutor.dto.LoginHistory;
 
@@ -20,11 +19,17 @@ public class AdLoginController {
 
     private final AdLoginHistoryService adLoginHistoryService;
 
+    @PostMapping("/loginHistory/deleteHistory")
+    public String deleteHistory(@RequestParam(value = "logNum") int logNum ) {
+
+
+        return "redirect:/admin/loginHistory";
+    }
+
     @GetMapping("/loginHistory")
     public String loginHistory(Model model) {
 
         List<LoginHistory> loginHistoryList = adLoginHistoryService.getAllLoginHistories();
-
         model.addAttribute("loginHistoryList", loginHistoryList);
         model.addAttribute("title", "로그인이력조회");
         model.addAttribute("content", "로그인이력조회");
--- src/main/java/team01/yaksutor/admin/service/AdLoginHistoryService.java
+++ src/main/java/team01/yaksutor/admin/service/AdLoginHistoryService.java
@@ -1,6 +1,7 @@
 package team01.yaksutor.admin.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import team01.yaksutor.admin.mapper.AdLoginHistoryMapper;
 import team01.yaksutor.dto.LoginHistory;
@@ -9,11 +10,13 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AdLoginHistoryService {
 
     private final AdLoginHistoryMapper adLoginHistoryMapper;
 
     public List<LoginHistory> getAllLoginHistories() {
-        //return adLoginHistoryMapper.get
+        List<LoginHistory> LoginHistoryList = adLoginHistoryMapper.getAllLoginHistories();
+        return LoginHistoryList;
     }
 }
