2b6d6bc23b009c9f30ab213e1020fab9f0074023
jyPark4321 <qkrwnddus93@gmail.com>
2024-07-17 17:30:00 +0900

    feat : 장바구니에서 주문 기능 끝

--- src/main/java/team01/yaksutor/dto/OrderDetail.java
+++ src/main/java/team01/yaksutor/dto/OrderDetail.java
@@ -8,7 +8,10 @@ public class OrderDetail {
     private String orderDetailCode; // 주문 상세 코드
     private String oCode; // 주문 코드
     private String goodsCode; // 등록 의약품 코드
+    private String oSellerId; //판매자Id
+    private String suppCode; //납품업체코드
     private int orderQty; // 주문 수량
     private int orderPrice; // 구매 액수
+
 
 }
--- src/main/java/team01/yaksutor/pharmacy/mapper/PhOrderMapper.java
+++ src/main/java/team01/yaksutor/pharmacy/mapper/PhOrderMapper.java
@@ -1,9 +1,19 @@
 package team01.yaksutor.pharmacy.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import team01.yaksutor.dto.Order;
+import team01.yaksutor.dto.OrderDetail;
 import team01.yaksutor.dto.SellerInfo;
 
 @Mapper
 public interface PhOrderMapper {
     SellerInfo getSellerInfoByGoodsCode(String goodsCode);
+
+    void addOrder(Order order);
+
+    void addOrderDetail(OrderDetail orderDetail);
+
+    void updateOrderPrice(String oCode, int price);
+
+    void updateOrderPrice(Order order);
 }
